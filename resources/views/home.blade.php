5dee809eff628fec2a3b54b7d44c9f76cfb440cc
stasa <magarence@gmail.com>
2016-04-14 21:41:36 +0200

    poslednji pred novi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
 			<input type="text" name="name"/>
 			<label for="niceness">Niceness</label>
 			<input type="text" name="niceness"/>
-			<button type="submit">Do a nice action</button>
+			<button type="submit" onclick="send(event)">Do a nice action</button>
 			<input type="hidden" value="{{ Session::token() }}" name="_token">
 		</form>
 		<br><br><br>
@@ -31,7 +31,32 @@
 				@endforeach
 				</li>
 			@endforeach
-
 		</ul>
+		@if($logged_actions->lastPage() > 1)
+			@for($i = 1; $i <= $logged_actions->lastPage(); $i++)
+				<a href="{{ $logged_actions->url($i) }}">{{ $i }}</a>
+			@endfor
+		@endif
+	<script type="text/javascript">
+		function send(event) {
+			event.preventDefault();
+			$.ajax({
+				url: "{{ route('add_action') }}",
+				type: 'POST',
+				data: $("form").serialize()
+
+				//{name: $('name').val(), niceness: $('niceness').val(), _token: "{{ Session::token() }}"}
+			})
+
+			.fail(function(data) {
+
+        // show any errors
+        // best to remove for production
+        console.log(data);
+    });
+
+		}
+	</script>
+
 	</div>
 @endsection
